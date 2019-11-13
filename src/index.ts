02a6a60b2be8bc5c89ca5ec15601d3362b8f16cc
Miloš Stojanović <milos.stojanovic@global-engineering-technologies.com>
2019-11-13 23:07:01 +0100

    Initial project commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import service from './service';
+
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send(service);
+});
+
+// start the Express server
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server started at http://localhost:${port}`);
+});
