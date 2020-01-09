7c3679e6730401584dcd2ac6518c58a201197a42
Miloš Stojanović <milos.stojanovic@global-engineering-technologies.com>
2020-01-09 21:27:09 +0100

    Scaffold development mode.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import service from './service';
 
 const app = express();
 const port = 8080; // default port to listen
 
+app.use(
+  cors({
+    origin: 'http://localhost:5000',
+    optionsSuccessStatus: 200,
+  }),
+);
+
 // define a route handler for the default home page
-app.get('/', (req, res) => {
-  res.send(service);
+app.get('/hello', (req, res) => {
+  res.json({ data: service });
 });
+
+// app.use('/', express.static(path.join(__dirname, 'public')));
 
 // start the Express server
 app.listen(port, () => {
