97208e416e19316f9be1b69a578e4168b2119abd
Miloš Stojanović <milos.stojanovic@global-engineering-technologies.com>
2020-01-09 22:44:36 +0100

    Scaffold production build.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import service from './service';
 
 const app = express();
 const port = 8080; // default port to listen
 
-app.use(
-  cors({
-    origin: 'http://localhost:5000',
-    optionsSuccessStatus: 200,
-  }),
-);
+const { APP_ENV } = process.env;
+
+if (APP_ENV === 'development') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5000',
+      optionsSuccessStatus: 200,
+    }),
+  );
+} else {
+  app.use('/', express.static(path.join(__dirname, 'public')));
+}
 
 // define a route handler for the default home page
 app.get('/hello', (req, res) => {
   res.json({ data: service });
 });
-
-// app.use('/', express.static(path.join(__dirname, 'public')));
 
 // start the Express server
 app.listen(port, () => {
